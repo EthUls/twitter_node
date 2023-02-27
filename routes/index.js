a991099c239e1a4a8c6c72ddba9d0adbff7a6dc4
Guillaume <guillaume.darlin7@yahoo.com>
2023-02-27 11:25:46 +0100

    feat: Model, routes controllers pour users

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-const router = require('express').Router()
-const tweet = require('./tweet.routes')
+const router = require('express').Router();
+const tweetRoute = require('./tweet.routes');
+const userRoute = require('./user.routes');
 const { tweetList } = require('../controllers/tweet.controller');
 
 // PAR DEFAUT, LE LANCEMENT SE FAIT SUR INDEX.JS
-router.use('/tweet', tweet); // Si l'utilisateur fait un "/tweet" il se retrouvera sur la route /tweet.route.js
+router.use('/tweet', tweetRoute); // Si l'utilisateur fait un "/tweet" il se retrouvera sur la route /tweet.route.js
+router.use('/user', userRoute);
+
+
 router.get('/', tweetList);
 
 module.exports = router;
