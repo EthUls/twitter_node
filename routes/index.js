2290747677a0033e905ff3c5e90332db7e96ce05
Guillaume <guillaume.darlin7@yahoo.com>
2023-02-28 10:09:10 +0100

    feat: Configuration de passport

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 const tweetRoute = require('./tweet.routes');
 const userRoute = require('./user.routes');
+const authRoute = require('./auth.routes');
 const { tweetList } = require('../controllers/tweet.controller');
 
 // PAR DEFAUT, LE LANCEMENT SE FAIT SUR INDEX.JS
 router.use('/tweet', tweetRoute); // Si l'utilisateur fait un "/tweet" il se retrouvera sur la route /tweet.route.js
 router.use('/user', userRoute);
-
+router.use('/auth', authRoute);
 
 router.get('/', tweetList);
 
