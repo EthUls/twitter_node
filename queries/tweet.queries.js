b1ef8fdce3b51b347a3a5d77eb0a4d7e62a69998
Guillaume <guillaume.darlin7@yahoo.com>
2023-03-01 09:50:24 +0100

    feat: Modification d'un Tweet

--- queries/tweet.queries.js
+++ queries/tweet.queries.js
@@ -11,6 +11,13 @@ exports.findAllTweets = () => {
 }
 
 exports.findTweetAndDelete = (tweetId) => {
-
     return Tweet.findByIdAndDelete(tweetId).exec();
+}
+
+exports.findTweetById = (tweetId) => {
+    return Tweet.findById(tweetId).exec();
+}
+
+exports.findTweetAndUpdate = (tweetId, body) => {
+    return Tweet.findByIdAndUpdate(tweetId, {$set: body}).exec();
 }
--- routes/tweet.routes.js
+++ routes/tweet.routes.js
@@ -1,7 +1,9 @@
-const { createTweet, deleteTweet } = require('../controllers/tweet.controller');
+const { createTweet, deleteTweet, displayTweet, updateTweet} = require('../controllers/tweet.controller');
 const router = require('express').Router();
 
 router.post('/new', createTweet) //creation d'un tweet
 router.get('/delete/:tweetId', deleteTweet); //suprimer un tweet
+router.get('/edit/:tweetId', displayTweet); //Affiche le tweet pour le modifier
+router.post('/edit/:tweetId', updateTweet); //Mise a jour du tweet
 
 module.exports = router
