b64111740b0d845b89073528b3526f92c7ede00c
Guillaume <guillaume.darlin7@yahoo.com>
2023-03-01 10:32:09 +0100

    feat: MiddleWare Ensure Authenticated

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,10 @@ const tweetRoute = require('./tweet.routes');
 const userRoute = require('./user.routes');
 const authRoute = require('./auth.routes');
 const { tweetList } = require('../controllers/tweet.controller');
+const { ensureAuthenticated } = require('../config/security.config');
 
 // PAR DEFAUT, LE LANCEMENT SE FAIT SUR INDEX.JS
-router.use('/tweet', tweetRoute); // Si l'utilisateur fait un "/tweet" il se retrouvera sur la route /tweet.route.js
+router.use('/tweet', ensureAuthenticated, tweetRoute); // Si l'utilisateur fait un "/tweet" il se retrouvera sur la route /tweet.route.js
 router.use('/user', userRoute);
 router.use('/auth', authRoute);
 
--- routes/tweet.routes.js
+++ routes/tweet.routes.js
@@ -1,7 +1,7 @@
 const { createTweet, deleteTweet, displayTweet, updateTweet} = require('../controllers/tweet.controller');
 const router = require('express').Router();
 
-router.post('/new', createTweet) //creation d'un tweet
+router.post('/new' ,createTweet) //creation d'un tweet
 router.get('/delete/:tweetId', deleteTweet); //suprimer un tweet
 router.get('/edit/:tweetId', displayTweet); //Affiche le tweet pour le modifier
 router.post('/edit/:tweetId', updateTweet); //Mise a jour du tweet
